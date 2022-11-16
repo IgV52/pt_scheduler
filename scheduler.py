fee39c450bcdac0646394c304ce526a5139a1407
Ivan Grubov <qalbertooq@mail.ru>
2022-11-16 23:44:26 +0300

    Монополия - Запрещено одновременное использование ресурса для записи (Task.write_set) разными задачами

--- scheduler.py
+++ scheduler.py
@@ -5,11 +5,12 @@ Task = namedtuple('Task', 'name priority target_set write_set read_set')
 
 
 class Scheduler:
-    STAGE = 3
+    STAGE = 4
 
     def __init__(self):
+        self._task_process = None
         self._task_queue = None
-        self._task_procces = None
+        self._write_res_block = None
 
     @property
     def content(self):
@@ -28,8 +29,9 @@ class Scheduler:
 
     def get(self):
         if self._task_queue:
-            self._sorted_priority
-            task_pop = self._task_queue.pop(0)
+            task_pop = self._iter_variable()
+            self._add_write_res_block(task_pop.write_set)
+            self._task_queue.remove(task_pop)
             if not self._task_process:
                 self._task_process = [task_pop]
             else:
@@ -38,7 +40,31 @@ class Scheduler:
         return None
 
     def done(self, task):
-        pass
+        self._free_res(task)
+
+    def _add_write_res_block(self, write_set):
+        if not self._write_res_block:
+            self._write_res_block = {write for write in write_set}
+        else:
+            self._write_res_block.add(write_set)
+
+    def _free_res(self, task):
+        self._task_process.remove(task)
+        if self._write_res_block:
+            self._write_res_block.difference_update(task.write_set)
+
+    def _iter_variable(self):
+        self._sorted_priority()
+        for task in self._task_queue:
+            if self._search_free_res(task):
+                return task
+
+    def _search_free_res(self, task):
+        if self._write_res_block:
+            for block_res in self._write_res_block:
+                if  block_res in task.write_set:
+                    return False
+        return True
 
     def _sorted_priority(self):
         self._task_queue = sorted(self._task_queue, 
